58fec34f41a16dedc8ff0f3f48037f3de203bef2
OxOtn3 <oxotn3@foxmail.com>
2022-02-07 22:11:44 +0800

    appid secret

--- shu-ssc-covid/src/main/java/com/shu/ssc/service/impl/LoginServiceImpl.java
+++ shu-ssc-covid/src/main/java/com/shu/ssc/service/impl/LoginServiceImpl.java
@@ -19,12 +19,15 @@ import javax.annotation.Resource;
 @Service
 public class LoginServiceImpl implements LoginService {
 
+    public static final String appid = "wx065ffe4ebe7ecbb0";
+    public static final String secret = "8da2fc11e4cc640adea574d125772c7e";
+
     @Resource
     RestTemplate restTemplate;
 
     @Override
     public LoginResponseDto login(String code) throws Exception {
-        String getUrl = "https://api.weixin.qq.com/sns/jscode2session?appid=wx346080c58fc7de16&secret=c156b608d31cf4c47358d5ee0c218109&js_code=" + code + "&grant_type=authorization_code";
+        String getUrl = "https://api.weixin.qq.com/sns/jscode2session?appid=" + appid + "&secret=" + secret + "&js_code=" + code + "&grant_type=authorization_code";
         System.out.println("getUrl: " + getUrl);
         ResponseEntity<String> re = restTemplate.getForEntity(getUrl, String.class);
         System.out.println("====== etForEntity(getUrl, String.class" + re.getBody());
